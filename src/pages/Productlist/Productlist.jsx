972445583c435855b3f8c91975c8cb7579f7c8de
rohan <rohanthakur89768@gmail.com>
2024-03-09 19:40:38 +0530

    added filtercomponnet

--- src/pages/Productlist/Productlist.jsx
+++ src/pages/Productlist/Productlist.jsx
@@ -1,12 +1,15 @@
 import './Productlist.css'
 import Productimg from '../../assets/product.avif'
 import Productbox from '../../components/Navbar/Productbox/Productbox';
+import FilterProduct from '../../components/Filterproduct/FilterProduct';
 export default function Productlist(){
 return(
      <div className='container'>
         <div className='row'>
             <h2 className="product-list-title text-center">All Products</h2>
             <div className="product-list-wrapper d-flex flex-row">
+
+                <FilterProduct/>
           
            {/* list of products */}
             <div className="product-list-box" id="productList">
